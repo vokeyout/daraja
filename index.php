389375a5627d547d223366507aa2f82140818454
Kelvin <vokeyyutt@gmail.com>
2023-12-20 15:53:35 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Portable WIFI High Speed ​​4G</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+    
+    <div class="row">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Networking Devices</a></li>
+            <li class="breadcrumb-item"><a href="#">Routers</a></li>
+        </ul>
+        
+        <div class="col-sm">
+            <img class="img-fluid" src="img/mifi1.jpg" alt="Portable_WIFI">
+            
+     
+            <div class="mt-4 p-5 border border-dark rounded">
+            <form action="/action_page.php">
+                <h3>Ksh. 299.00</h3>
+                <p class="text-success"><strong>In Stock</strong></p> 
+                <p></p>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Quantity</span>
+                    <input type="text" class="form-control">
+                </div>
+            </form>
+             <!--Large modals-->
+                <div class="container mt-3">                              
+                    <button type="button" class="btn btn-lg btn-warning" data-bs-toggle="modal" data-bs-target="#myModal">
+                    Pay Now
+                    </button>
+                </div>
+                
+                <!-- The Modal -->
+                <div class="modal" id="myModal">
+                    <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                
+                        <!-- Modal Header -->
+                        <div class="modal-header">                        
+                        <h4 class="modal-title">Portable WIFI High Speed ​​4G Mobile Hotspot Router Support 10 Users LED Smart Display 2100mAh Battery for Phone Laptop Desktop Tablet White</h4>                       
+                        </div>
+                
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                        <img class="img-fluid" src="img/mifi32.jpg" alt="Portable_WIFI">
+                        <h3>Ksh. 299.00</h3>
+                        <p class="text-success"><strong>In Stock</strong></p>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Quantity</span>
+                            <input type="text" class="form-control">
+                        </div>
+                        <button type="button" class="btn btn-lg btn-warning">Pay with  </button> 
+                        <img src="img/mpesa.svg" alt="mpesa">
+                        </div>
+                
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        </div>
+                
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col-sm mt-4">
+            <h2>Portable WIFI High Speed ​​4G Mobile Hotspot Router Support 10 Users LED Smart Display 2100mAh Battery for Phone Laptop Desktop Tablet White</h2>           
+            <p class="text-primary">Brand: Tonysa</p>
+            <hr>
+            <p class="text-success"><strong>In stock</strong></p>
+            
+            <ul>
+                <li><strong>Brand:</strong> Tonysa</li>
+                <li><strong>Model name:</strong> Tonysa5gc91mki6t</li>
+                <li><strong>Wireless communication standard:</strong> 802.11b</li>
+                <li><strong>Compatible devices:</strong> Laptop, Personal Computer, Tablet, Smartphone</li>
+                <li><strong>Connectivity technology:</strong> Wi-Fi</li>
+                <li><strong>Antenna type:</strong> Internal</li>
+                <li><strong>Operating system:</strong> Windows7 8</li>
+                <li><strong>Number of ports:</strong> 1</li>
+                <li><strong>Control method:</strong> Touch</li>
+                <li><strong>Maximum upstream data transfer rate:</strong> 50 Megabits Per Second</li>
+                
+            </ul>
+            <hr>
+            <h3>About this item</h3>
+            <ul>
+                <li>High Speed ​​Internet Access: The 4G Mobile WiFi Cellular Router is designed to provide users with high speed 3G 4G Internet access and support the 3G 4G main frequency band of most countries and operators in the world.</li>
+                <li>Support: Mobile WiFi is mainly intended for drivers, business travelers and outdoor travelers, and supports 10 users at the same time. Support for Windows7 8 10.</li>
+                <li>Lightweight and Portable: Lightweight, thin and compact design makes it easy to carry; Whether you're working from home or traveling, the 4G Travel LTE Router enables your WiFi to follow your steps around the world.</li>
+                <li>2100mAh large capacity battery: standby up to 72 hours, faster, more durable and more portable SIM card insertion function, enjoy 4G mobile internet, WiFi LED smart display, easy to use.</li>
+                <li>Wide Compatibility: Just insert a standard SIM card, no matter mobile phone, laptop, desktop or tablet, you can easily connect to WiFi (SIM card not included).</li>
+            </ul>
+
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
